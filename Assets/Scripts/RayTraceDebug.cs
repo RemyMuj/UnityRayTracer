4700cef95bff6f44f32e60afe8dc961664a7ad2a
Remy <themememime@gmail.com>
2024-09-03 20:31:15 -0600

    Debug Ray Intersection Drawing

--- Assets/Scripts/RayTraceDebug.cs
+++ Assets/Scripts/RayTraceDebug.cs
@@ -8,8 +8,12 @@ public class RayTraceDebug : MonoBehaviour {
     public int debugLevel = 2; // 0 - NONE, 1 - WARNINGS ONLY, 2 - BASIC, 3 - DETAILED
     public bool drawSphereTree;
     public bool drawMeshTree;
-    public bool drawMouseRayTrace;
+    public bool drawRayTrace;
+    public Vector3 testRay;
+    private Vector3 startRay;
     public bool drawNormals;
+    private const float EPSILON = float.Epsilon;
+    public const float FLOAT_MAX = 3.40282347E+38f;
 
     /// Awake(): Setup ///
     private void Awake() {
@@ -62,14 +66,42 @@ public class RayTraceDebug : MonoBehaviour {
         Gizmos.DrawLine(corner3, corner5);
     }
 
+    /// IntersectBVHNode(): Axis-Aligned Bounding Box intersection test (For debug drawing) ///
+    private bool IntersectBVHNode(Vector3 start, Vector3 end, RayTraceMaster.BVHNode node) {
+        // Calculate mutual time range to cross extent boundaries
+        float t_min = -FLOAT_MAX;
+        float t_max = FLOAT_MAX;
+        float t1, t2;
+
+        // change to direction vector
+        end = end - start;
+
+        for (int i = 0; i < 3; i++) { // 0 - x, 1 - y, 2 - z
+            t1 = (node.vmin[i] - start[i]) / (end[i] + EPSILON);
+            t2 = (node.vmax[i] - start[i]) / (end[i] + EPSILON);
+
+            t_min = Mathf.Max(t_min, Mathf.Min(t1, t2));
+            t_max = Mathf.Min(t_max, Mathf.Max(t1, t2));
+        }
+
+        // Return
+        return (t_max >= t_min);
+    }
+
     /// DrawBVH(): Draw BVH bounds ///
     private void DrawBVH(List<RayTraceMaster.BVHNode> list, int depth, Color col, Color colChange, int index = 0) {
         if (depth > 0) {
             // Get node
             RayTraceMaster.BVHNode node = list[index];
 
-            // Draw Bounding Volume
+            // Decide on Box Color
             Gizmos.color = col;
+            if (drawRayTrace && IntersectBVHNode(startRay, testRay, node)) {
+                // Highlight if bounds intersects
+                Gizmos.color = Color.black;
+            }
+
+            // Draw Bounding Volume
             DrawBox(node.vmin, node.vmax);
 
             // Draw info (position in tree list, index of object)
@@ -82,6 +114,22 @@ public class RayTraceDebug : MonoBehaviour {
             DrawBVH(list, depth - 1, col + colChange, colChange, index*2 + 1);
             DrawBVH(list, depth - 1, col + colChange, colChange, index*2 + 2);
         }
+    }
+
+    /// DrawRayTrace(): Draw ray test trace from camera and highlight Bounds it triggers ///
+    private int DrawRayTrace() {
+        // Condition check
+        if (!drawRayTrace) return 1;
+
+        // Update Test Ray (WIP)
+        startRay = GetComponent<Transform>().position;
+
+        // Draw Test Ray
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(startRay, startRay + testRay);
+
+        // Default return
+        return 0;
     }
 
     /// DrawBVHTree(): Fully Draw BVH Tree ///
@@ -97,17 +145,17 @@ public class RayTraceDebug : MonoBehaviour {
             case 1: // Sphere BVH Tree
                 if (!drawSphereTree) return 1;
                 startCol = new Color(1.0f, 0.0f, 0.0f);
-                endCol = new Color(0.0f, 1.0f, 1.0f);
+                endCol = new Color(1.0f, 1.0f, 0.0f);
                 break;
             default: // Invalid Tree Type
                 return 1;
         }
 
-        // Basic Drawing
-        DrawBVH(list, depth, startCol, (endCol - startCol) / depth);
+        // Test Ray Tracing
+        DrawRayTrace();
 
-        // Mouse Ray Trace
-        // BLANK
+        // BVH Drawing
+        DrawBVH(list, depth, startCol, (endCol - startCol) / depth);
 
         // Default Return
         return 0;
